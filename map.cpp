f3dd49f9365426c8490461d933d0c7f16cf79900
허태정 <gjdigj@naver.com>
2019-06-05 22:11:51 +0900

    map 클래스 vector구현, checkMapdata(테스트) 확인

--- map.cpp
+++ map.cpp
@@ -1,69 +1,72 @@
 #include "map.h"
+#include <sstream>
 
 void map::initMap()
 {
     ifstream reader("maps.txt");
     string temp;
     getline(reader, temp);
-    num_maps = temp[0] - '0'; // 맵의 개수
+    num_maps = atoi(temp.c_str());
+    cout << "맵 데이터의 수 : " << num_maps << endl;
+
     int c_col, c_row;   // 캐릭터 위치 정보
     char start_symbol;  // @
 
-    maps = new int **[num_maps];
     chracters = new Point[num_maps]; // 캐릭터 위치 배열
     map_sizes = new Point[num_maps]; // 맵 사이즈 배열
-    string line[MAX_LINE];           // 줄별로 받아온 데이터
-
+    vector<string> line;           // 줄별로 받아온 데이터
+    
     for (int i = 0; i < num_maps; i++)
     {
         int row = 0, col = 0; // 맵 사이즈
+        line = vector<string>(); // 벡터 초기화
 
         getline(reader, temp);
         c_col = temp[0];
         c_row = temp[2];
         chracters[i] = Point(c_col, c_row);
 
-        while (1)
-        {
-            getline(reader, line[row]);
-            if (line[row] == "@")
-            { // 맵 구분자
+        while (1) {
+            getline(reader, temp);
+            line.push_back(temp);
+            if (line.back() == "@") { // 맵 구분자
                 break;
             }
-            cout << line[row] << endl;
-            row++;
+            // cout << line.back() << endl;
         }
 
+        line.pop_back(); // 맵 구분자를 벡터에서 제거
+        row = line.size(); // 줄 수
         col = line[0].size() / 2 + 1; // 공백 제거
         map_sizes[i] = Point(col, row);
-        cout << "row : " << row << " col : " << col << endl;
 
+        cout << "row : " << row << " col : " << col << endl;
+        if(i==0){ // map 초기화
+            maps.assign(num_maps, vector<vector<int>>(row, vector<int>(col, 0)));
+        }
         setMap(i, col, row, line);
     }
 }
 
-void map::setMap(int level, int col, int row, string *temp)
-{
-    maps[level] = new int *[row]; // 단일 맵 데이터
-    // 맵을 temp값을 해석해서 저장
-    for (int i = 0; i < row; i++)
+void map::setMap(int level, int col, int row, vector<string> temp) {
+    string tempString;
+    vector<vector<int>> &map = maps[level];
+    for (int i = 0; i < temp.size(); i++)
     {
-        maps[level][i] = new int[col];
-        cout << temp[i] << endl;
-        for (int j = 0; j < temp[i].size(); j++)
-        {
-            cout << j << endl;
-            if (temp[i][j] != ' ')
-            {
-                int n = temp[i][j] - '0';
-                maps[level][i][j] = n;
+        stringstream stream;
+        stream.str(temp[i]);
+        int idx = 0;
+        while(stream >> tempString){
+            if (tempString != " ") {
+                int n = stoi(tempString);
+                map[i][idx] = n;
+                idx++;
             }
         }
-        cout << endl;
     }
 }
 
-int **map::getMap(int level)
+vector<vector<int>> map::getMap(int level)
 {
     return maps[level];
 }
@@ -85,11 +88,11 @@ int map::getMapNum()
 
 void map::checkMapData(int level)
 {
-    int **map = maps[level];
+    vector<vector<int>> map = maps[level];
     Point map_size = map_sizes[level];
-    for (int row = 0; row < map_size.row-1; row++)
+    for (int row = 0; row < map_size.row; row++)
     {
-        for (int col = 0; col < map_size.col-1; col++)
+        for (int col = 0; col < map_size.col; col++)
         {
             cout << map[row][col] << " ";
         }
@@ -101,4 +104,9 @@ int main()
 {
     map m;
     m.initMap();
+    int times = m.getMapNum();
+    for(int i=0; i<times; i++){
+        m.checkMapData(i);
+        cout << endl;
+    }
 }
--- map.h
+++ map.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
+#include <vector>
 using namespace std;
 /****************
 map클래스
@@ -22,17 +24,17 @@ class map
 {
 public:
 	int num_maps;
-	int ***maps;
+	vector<vector<vector<int>>> maps;
 	Point *chracters; // 캐릭터의 위치
 	Point *map_sizes; // 맵 사이즈
 
 	map(){};
-	void initMap();
-	void setMap(int, int, int, string temp[MAX_LINE]);
-	int** getMap(int);
-	Point getChracter(int);
-	Point getMapSize(int);
-	int getMapNum();
-	void checkMapData(int);
+	void initMap(); // 맵의 초기 설정, 맵 크기, 캐릭터의 위치
+	void setMap(int, int, int, vector<string>); // 맵 데이터를 실질적을 저장
+	vector<vector<int>> getMap(int); // 맵데이터
+	Point getChracter(int); // 캐릭터 위치
+	Point getMapSize(int); // 맵 사이즈
+	int getMapNum(); // 맵 개수
+	void checkMapData(int); // 맵 테스트
 };
 #endif
