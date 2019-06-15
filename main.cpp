8b2e3f67edd667acc04256dcd27a8ceade62a268
조익현 <cas10693@plustv.io>
2019-06-15 18:41:51 +0900

    no message

--- main.cpp
+++ main.cpp
@@ -151,8 +151,7 @@ void gameStart(int prefix){
 }
 void gameLoop(){
 	int a =0;
-	GameObject character = g.getCharater();
-	int c_x = character.p.col, c_y = character.p.row;
+	GameObject &character = g.getCharater();
 	while(!g.isStageEnd()){
 		int key = getch();
 
